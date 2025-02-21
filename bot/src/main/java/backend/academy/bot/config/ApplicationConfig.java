371fd36ee0f275fb044bcd095f0f2c6b99fe5504
moksem <maksim.buynov.00@mail.ru>
2025-02-21 12:29:34 +0300

    build: настройка общей конфигурации приложения

--- bot/src/main/java/backend/academy/bot/config/ApplicationConfig.java
+++ bot/src/main/java/backend/academy/bot/config/ApplicationConfig.java
@@ -1,9 +1,19 @@
-package backend.academy.bot;
+package backend.academy.bot.config;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
+@Getter
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record BotConfig(@NotEmpty String telegramToken) {}
+public class ApplicationConfig {
+
+    @NotEmpty
+    private String telegramToken;
+
+    public void setTelegramToken(String telegramToken) {
+        this.telegramToken = telegramToken;
+    }
+}
