bffc809d7cd13265bb96e156be8817325a88bebc
moksem <maksim.buynov.00@mail.ru>
2025-02-27 00:27:03 +0300

    feature: добавлен конфиг для rest клиента

---
+++ bot/src/main/java/backend/academy/bot/config/ClientConfig.java
@@ -0,0 +1,41 @@
+package backend.academy.bot.config;
+
+import backend.academy.bot.client.ChatClient;
+import backend.academy.bot.client.LinkClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class ClientConfig {
+
+    private final ApplicationConfig config;
+
+    @Autowired
+    public ClientConfig(ApplicationConfig config) {
+        this.config = config;
+    }
+
+    @Bean
+    public HttpServiceProxyFactory httpServiceProxyFactory(RestClient.Builder builder) {
+        RestClient restClient = builder
+            .baseUrl(config.scrapperUrl())
+            .build();
+        return HttpServiceProxyFactory
+            .builderFor(RestClientAdapter.create(restClient))
+            .build();
+    }
+
+    @Bean
+    public ChatClient chatClient(HttpServiceProxyFactory factory) {
+        return factory.createClient(ChatClient.class);
+    }
+
+    @Bean
+    public LinkClient linkClient(HttpServiceProxyFactory factory) {
+        return factory.createClient(LinkClient.class);
+    }
+}
