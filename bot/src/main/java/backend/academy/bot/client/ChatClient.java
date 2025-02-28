ffc1f83768d0aadf3b525e9a79d5abd6953b6838
moksem <maksim.buynov.00@mail.ru>
2025-02-28 12:33:08 +0300

    fix bot: исправлены http clients

--- bot/src/main/java/backend/academy/bot/client/ChatClient.java
+++ bot/src/main/java/backend/academy/bot/client/ChatClient.java
@@ -1,17 +1,18 @@
 package backend.academy.bot.client;
 
+import scrapper.bot.connectivity.model.ScrapperResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.service.annotation.DeleteExchange;
 import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
 
 @HttpExchange("/tg-chat/{id}")
 public interface ChatClient {
 
-    @PostMapping
-    ResponseEntity<?> registerChat(@PathVariable Long id);
+    @PostExchange
+    ResponseEntity<ScrapperResponse> registerChat(@PathVariable Long id);
 
-    @DeleteMapping
-    ResponseEntity<?> unregisterChat(@PathVariable Long id);
+    @DeleteExchange
+    ResponseEntity<ScrapperResponse> unregisterChat(@PathVariable Long id);
 }
