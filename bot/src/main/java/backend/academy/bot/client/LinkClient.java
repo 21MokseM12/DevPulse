e1f1d8862704987269dfa160796e8ddd59424ba7
moksem <maksim.buynov.00@mail.ru>
2025-02-28 12:33:08 +0300

    fix bot: исправлены http clients

--- bot/src/main/java/backend/academy/bot/client/LinkClient.java
+++ bot/src/main/java/backend/academy/bot/client/LinkClient.java
@@ -1,28 +1,29 @@
 package backend.academy.bot.client;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.service.annotation.DeleteExchange;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
 import scrapper.bot.connectivity.model.LinkRequest;
+import scrapper.bot.connectivity.model.ScrapperResponse;
 
 @HttpExchange("/links")
 public interface LinkClient {
 
-    @GetMapping
-    ResponseEntity<?> getAllLinks(@RequestHeader(name = "Tg-Chat-Id") Long chatId);
+    @GetExchange
+    ResponseEntity<ScrapperResponse> getAllLinks(@RequestHeader(name = "Tg-Chat-Id") Long chatId);
 
-    @PostMapping
-    ResponseEntity<?> subscribeLink(
+    @PostExchange
+    ResponseEntity<ScrapperResponse> subscribeLink(
         @RequestHeader(name = "Tg-Chat-Id") Long chatId,
         @RequestBody LinkRequest link
     );
 
-    @DeleteMapping
-    ResponseEntity<?> unsubscribeLink(
+    @DeleteExchange
+    ResponseEntity<ScrapperResponse> unsubscribeLink(
         @RequestHeader(name = "Tg-Chat-Id") Long chatId,
         @RequestBody String uri
     );
