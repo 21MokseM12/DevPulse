adba4cdfba25230519fe0b4b66f8d43bb16d250c
moksem <maksim.buynov.00@mail.ru>
2025-03-01 04:53:01 +0300

    fix: пакет команд теперь находится в корне проекта

--- bot/src/main/java/backend/academy/bot/config/BotConfig.java
+++ bot/src/main/java/backend/academy/bot/config/BotConfig.java
@@ -1,6 +1,6 @@
 package backend.academy.bot.config;
 
-import backend.academy.bot.model.commands.Command;
+import backend.academy.bot.commands.Command;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.request.SetMyCommands;
