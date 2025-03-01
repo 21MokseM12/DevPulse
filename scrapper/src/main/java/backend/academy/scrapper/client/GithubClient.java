c3ca3403afb440f6a4b4d06d42d68ef78f351187
moksem <maksim.buynov.00@mail.ru>
2025-03-01 04:55:28 +0300

    build: добавлены url github, url bot service, github access token

---
+++ scrapper/src/main/java/backend/academy/scrapper/client/GithubClient.java
@@ -0,0 +1,18 @@
+package backend.academy.scrapper.client;
+
+import backend.academy.scrapper.model.GithubResponse;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
+@HttpExchange("/repos/{name}/{repo}/events")
+public interface GithubClient {
+
+    @GetExchange
+    ResponseEntity<List<GithubResponse>> getEvents(
+        @PathVariable String name,
+        @PathVariable String repo
+    );
+}
