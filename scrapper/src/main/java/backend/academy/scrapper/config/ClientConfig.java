5e8fa039a89c72677e44f2ce375d7f419864b7e1
moksem <maksim.buynov.00@mail.ru>
2025-03-02 17:55:53 +0300

    fix: добавлены default ссылки, добавлена ссылка на stackoverflow api

--- scrapper/src/main/java/backend/academy/scrapper/config/ClientConfig.java
+++ scrapper/src/main/java/backend/academy/scrapper/config/ClientConfig.java
@@ -2,6 +2,7 @@ package backend.academy.scrapper.config;
 
 import backend.academy.scrapper.client.BotClient;
 import backend.academy.scrapper.client.GithubClient;
+import backend.academy.scrapper.client.StackOverflowClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +13,23 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 public class ClientConfig {
 
+    private static final String BASE_GITHUB_URL = "https://api.github.com";
+
+    private static final String BASE_BOT_URL = "https://api.github.com";
+
+    private static final String BASE_STACKOVERFLOW_URL = "https://api.stackexchange.com/2.3";
+
     @Autowired
     private ScrapperConfig scrapperConfig;
 
     @Bean
-    public GithubClient getGithubClient(RestClient.Builder builder) {
+    public GithubClient githubClient(RestClient.Builder builder) {
         RestClient restClient = builder
-            .baseUrl(scrapperConfig.github().url())
+            .baseUrl(
+                scrapperConfig.github().url() == null ?
+                    BASE_GITHUB_URL :
+                    scrapperConfig.github().url()
+            )
             .defaultHeader("Authorization", "token " + scrapperConfig.github().token())
             .build();
         return HttpServiceProxyFactory
@@ -28,9 +39,28 @@ public class ClientConfig {
     }
 
     @Bean
+    public StackOverflowClient stackOverflowClient(RestClient.Builder builder) {
+        RestClient restClient = builder
+            .baseUrl(
+                scrapperConfig.stackOverflow().url() == null ?
+                    BASE_STACKOVERFLOW_URL :
+                    scrapperConfig.stackOverflow().url()
+            )
+            .defaultHeader("Authorization", "token " + scrapperConfig.github().token())
+            .build();
+        return HttpServiceProxyFactory
+            .builderFor(RestClientAdapter.create(restClient))
+            .build()
+            .createClient(StackOverflowClient.class);
+    }
+
+    @Bean
     public BotClient botClient(RestClient.Builder builder) {
         RestClient restClient = builder
-            .baseUrl(scrapperConfig.botUrl())
+            .baseUrl(
+                scrapperConfig.botUrl() == null ?
+                    BASE_BOT_URL :
+                    scrapperConfig.botUrl())
             .build();
         return HttpServiceProxyFactory
             .builderFor(RestClientAdapter.create(restClient))
