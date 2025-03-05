1afee64c5bf9b0f1b359b28782593e18a7c353dd
moksem <maksim.buynov.00@mail.ru>
2025-03-05 21:13:05 +0300

    fix linters

--- scrapper/src/main/java/backend/academy/scrapper/client/StackOverflowClient.java
+++ scrapper/src/main/java/backend/academy/scrapper/client/StackOverflowClient.java
@@ -1,11 +1,11 @@
 package backend.academy.scrapper.client;
 
 import backend.academy.scrapper.model.StackOverflowResponse;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
-import java.util.List;
 
 @HttpExchange("/questions/{questionId}")
 public interface StackOverflowClient {
