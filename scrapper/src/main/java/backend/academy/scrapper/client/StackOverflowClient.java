290751e6b53a188c098f991aa8442da7a0ab7851
moksem <maksim.buynov.00@mail.ru>
2025-03-06 17:00:06 +0300

    fix: исправлены сущности при отправке запроса на stackoverflow, исправлена конфигурация самого запроса

--- scrapper/src/main/java/backend/academy/scrapper/client/StackOverflowClient.java
+++ scrapper/src/main/java/backend/academy/scrapper/client/StackOverflowClient.java
@@ -1,15 +1,20 @@
 package backend.academy.scrapper.client;
 
 import backend.academy.scrapper.model.StackOverflowResponse;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
-import java.util.List;
 
 @HttpExchange("/questions/{questionId}")
 public interface StackOverflowClient {
 
     @GetExchange
-    ResponseEntity<List<StackOverflowResponse>> getEvents(@PathVariable Long questionId);
+    ResponseEntity<StackOverflowResponse> getEvents(
+        @PathVariable Long questionId,
+        @RequestParam String order,
+        @RequestParam String sort,
+        @RequestParam String site);
 }
