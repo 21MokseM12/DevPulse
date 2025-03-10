e728cdf55383409dec8e42f443576ebe928112c2
moksem <maksim.buynov.00@mail.ru>
2025-03-11 00:49:00 +0300

    fix: исправлены тесты контроллеров

--- scrapper/src/test/java/backend/academy/scrapper/controller/ChatControllerTest.java
+++ scrapper/src/test/java/backend/academy/scrapper/controller/ChatControllerTest.java
@@ -1,13 +1,6 @@
 package backend.academy.scrapper.controller;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import backend.academy.scrapper.service.ChatService;
+import backend.academy.scrapper.database.ChatService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +8,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ChatController.class)
 @RunWith(SpringRunner.class)
