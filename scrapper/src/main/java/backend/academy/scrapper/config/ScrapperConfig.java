c3f8b45c2551db32b9b7bcd900807c1d617a9e30
moksem <maksim.buynov.00@mail.ru>
2025-03-15 00:32:30 +0300

    fix: изменена структура yml файла

--- scrapper/src/main/java/backend/academy/scrapper/config/ScrapperConfig.java
+++ scrapper/src/main/java/backend/academy/scrapper/config/ScrapperConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
-@ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
+@ConfigurationProperties(prefix = "app.scrapper", ignoreUnknownFields = false)
 public record ScrapperConfig(
         GitHubCredentials github,
         StackOverflowCredentials stackOverflow,
--- scrapper/src/main/java/backend/academy/scrapper/config/access/JdbcAccessConfig.java
+++ scrapper/src/main/java/backend/academy/scrapper/config/access/JdbcAccessConfig.java
@@ -4,15 +4,15 @@ import backend.academy.scrapper.database.ChatService;
 import backend.academy.scrapper.database.LinkService;
 import backend.academy.scrapper.database.jdbc.JdbcChatService;
 import backend.academy.scrapper.database.jdbc.JdbcLinkService;
-import backend.academy.scrapper.database.repository.jdbc.JdbcChatRepository;
-import backend.academy.scrapper.database.repository.jdbc.JdbcLinkRepository;
-import backend.academy.scrapper.database.repository.jdbc.JdbcLinkToChatRepository;
+import backend.academy.scrapper.database.jdbc.repository.JdbcChatRepository;
+import backend.academy.scrapper.database.jdbc.repository.JdbcLinkRepository;
+import backend.academy.scrapper.database.jdbc.repository.JdbcLinkToChatRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnProperty(prefix = "app", name = "database-access-type", havingValue = "SQL")
+@ConditionalOnProperty(prefix = "app.database", name = "access-type", havingValue = "SQL")
 public class JdbcAccessConfig {
 
     @Bean
