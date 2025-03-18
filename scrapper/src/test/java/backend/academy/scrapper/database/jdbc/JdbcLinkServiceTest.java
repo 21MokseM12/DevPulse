1f32eb3179ab5cc3562a07ad6e47d0f3a05bf5e4
moksem <maksim.buynov.00@mail.ru>
2025-03-18 06:44:22 +0300

    test: добавлены тесты, поднятие тест контейнеров

---
+++ scrapper/src/test/java/backend/academy/scrapper/database/jdbc/JdbcLinkServiceTest.java
@@ -0,0 +1,144 @@
+package backend.academy.scrapper.database.jdbc;
+
+import backend.academy.scrapper.database.jdbc.mapper.LinkResponseMapper;
+import backend.academy.scrapper.database.jdbc.repository.JdbcChatRepository;
+import backend.academy.scrapper.database.jdbc.repository.JdbcLinkRepository;
+import backend.academy.scrapper.database.jdbc.repository.JdbcLinkToChatRepository;
+import backend.academy.scrapper.database.model.Link;
+import java.net.URI;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import scrapper.bot.connectivity.model.request.AddLinkRequest;
+import scrapper.bot.connectivity.model.request.RemoveLinkRequest;
+import scrapper.bot.connectivity.model.response.LinkResponse;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class JdbcLinkServiceTest {
+
+    @Mock
+    private JdbcLinkRepository linkRepository;
+
+    @Mock
+    private JdbcChatRepository chatRepository;
+
+    @Mock
+    private JdbcLinkToChatRepository linkToChatRepository;
+
+    @InjectMocks
+    private JdbcLinkService linkService;
+
+    @Test
+    public void testGetAllLinksSuccess() {
+        Long id = 1L;
+        List<Link> links =
+            List.of(new Link(1L, URI.create("uri"), Set.of("tag"), Set.of("filter"), OffsetDateTime.now()));
+        List<LinkResponse> response =
+            List.of(new LinkResponse(1L, URI.create("uri"), Set.of("tag"), Set.of("filter")));
+
+        when(chatRepository.isClient(id)).thenReturn(true);
+        when(linkToChatRepository.findAllIdByChatId(id)).thenReturn(List.of(id));
+        when(linkRepository.findAllLinks(List.of(id))).thenReturn(links);
+        List<LinkResponse> byChatId = linkService.findAllByChatId(id);
+
+        assertThat(!byChatId.isEmpty()).isTrue();
+        assertThat(byChatId.size()).isEqualTo(links.size());
+        assertThat(byChatId.getFirst()).isEqualTo(response.getFirst());
+        verify(linkRepository).findAllLinks(List.of(id));
+        verify(chatRepository).isClient(id);
+        verify(linkToChatRepository).findAllIdByChatId(id);
+    }
+
+    @Test
+    public void testGetAllLinksFailure() {
+        Long id = 1L;
+        List<Long> list = List.of(id);
+        when(linkToChatRepository.findAllIdByChatId(id)).thenReturn(list);
+        when(linkRepository.findAllLinks(list)).thenReturn(List.of());
+        when(chatRepository.isClient(id)).thenReturn(true);
+
+        List<LinkResponse> byChatId = linkService.findAllByChatId(id);
+
+        assertThat(byChatId.isEmpty()).isTrue();
+        verify(linkRepository).findAllLinks(list);
+        verify(chatRepository).isClient(id);
+        verify(linkToChatRepository).findAllIdByChatId(id);
+    }
+
+    @Test
+    public void whenLinkIsNotRegistered_thenSaveLinkAndSubscribeAccount() {
+        Long id = 123L;
+        Long linkId = -1L;
+        AddLinkRequest addLinkRequest = new AddLinkRequest(
+            URI.create("link"),
+            Set.of("tag"),
+            Set.of("filter")
+        );
+        Link link = new Link(1L, URI.create("link"), Set.of("tag"), Set.of("filter"), OffsetDateTime.now());
+        LinkResponse expected = LinkResponseMapper.map(link);
+
+        when(chatRepository.isClient(id)).thenReturn(true);
+        when(linkRepository.findByLink(addLinkRequest.link().toString())).thenReturn(linkId);
+        when(linkRepository.save(addLinkRequest)).thenReturn(link);
+
+        Optional<LinkResponse> linkResponse = linkService.subscribe(id, addLinkRequest);
+        assertThat(linkResponse.isPresent()).isTrue();
+        assertEquals(expected, linkResponse.get());
+        verify(chatRepository).isClient(id);
+        verify(linkRepository).findByLink(addLinkRequest.link().toString());
+        verify(linkRepository).save(addLinkRequest);
+        verify(linkToChatRepository).subscribeChatOnLink(id, link.id());
+    }
+
+    @Test
+    public void whenLinkIsRegistered_thenOnlySubscribeAccount() {
+        Long id = 123L;
+        Long linkId = 1L;
+        AddLinkRequest addLinkRequest = new AddLinkRequest(
+            URI.create("link"),
+            Set.of("tag"),
+            Set.of("filter")
+        );
+        Link link = new Link(1L, URI.create("link"), Set.of("tag"), Set.of("filter"), OffsetDateTime.now());
+        LinkResponse expected = LinkResponseMapper.map(link);
+
+        when(chatRepository.isClient(id)).thenReturn(true);
+        when(linkRepository.findByLink(addLinkRequest.link().toString())).thenReturn(linkId);
+        when(linkRepository.findById(linkId)).thenReturn(link);
+
+        Optional<LinkResponse> linkResponse = linkService.subscribe(id, addLinkRequest);
+        assertThat(linkResponse.isPresent()).isTrue();
+        assertEquals(expected, linkResponse.get());
+        verify(chatRepository).isClient(id);
+        verify(linkRepository).findByLink(addLinkRequest.link().toString());
+        verify(linkRepository).findById(linkId);
+        verify(linkToChatRepository).subscribeChatOnLink(id, linkId);
+    }
+
+    @Test
+    public void testUnsubscribeLinkSuccess() {
+        Long id = 1L;
+        RemoveLinkRequest removeLinkRequest = new RemoveLinkRequest(URI.create("uri"));
+        Link link = new Link(1L, URI.create("uri"), Set.of(), Set.of(), OffsetDateTime.now());
+        LinkResponse linkResponse = new LinkResponse(1L, URI.create("uri"), Set.of(), Set.of());
+
+        when(chatRepository.isClient(id)).thenReturn(true);
+        when(linkRepository.existsLink(removeLinkRequest.link().toString())).thenReturn(true);
+        when(linkRepository.delete(removeLinkRequest.link().toString())).thenReturn(link);
+        Optional<LinkResponse> response = linkService.unsubscribe(id, removeLinkRequest);
+
+        assertThat(response.isPresent()).isTrue();
+        assertThat(response.get()).isEqualTo(linkResponse);
+        verify(linkRepository).delete(removeLinkRequest.link().toString());
+    }
+}
