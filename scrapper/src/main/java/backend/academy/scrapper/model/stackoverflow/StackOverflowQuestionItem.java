2b1491a45a3241ce7f37cf59e5904c479cbb8fce
moksem <maksim.buynov.00@mail.ru>
2025-03-26 09:46:36 +0300

    feature: изменена структура сущностей для работы с APIs

---
+++ scrapper/src/main/java/backend/academy/scrapper/model/stackoverflow/StackOverflowQuestionItem.java
@@ -0,0 +1,8 @@
+package backend.academy.scrapper.model.stackoverflow;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record StackOverflowQuestionItem(
+    @JsonProperty("title") String title
+) {
+}
