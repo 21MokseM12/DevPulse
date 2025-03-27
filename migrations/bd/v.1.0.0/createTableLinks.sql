be011d7fa88a70a181dfa2554038a2e76bc8bdc5
moksem <maksim.buynov.00@mail.ru>
2025-03-27 07:37:31 +0300

    migrations: добавлены индексы на поля, по которым производится поиск

--- migrations/bd/v.1.0.0/createTableLinks.sql
+++ migrations/bd/v.1.0.0/createTableLinks.sql
@@ -2,4 +2,7 @@ CREATE TABLE links (
     id BIGSERIAL PRIMARY KEY,
     link TEXT NOT NULL,
     updated_at TIMESTAMP NOT NULL
-)
+);
+
+CREATE INDEX link_value_index ON links (link);
+CREATE INDEX link_updated_time_index ON links (updated_at);
